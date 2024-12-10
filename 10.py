1679fc99f1f21dd8bd862482191bef35ab3dfbe8
Julius Franke <julius.franke@gmx.net>
2024-12-10 17:54:43 +0100

    day 10.2

--- 10.py
+++ 10.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Dict, List, Tuple
+from typing import Callable, Dict, List, Tuple
 from tqdm import tqdm
 from pathlib import Path
 from collections import deque, OrderedDict
@@ -27,7 +27,7 @@ ans_1 = 0
 ans_2 = 0
 
 
-def recursive_search(pos: Tuple, num: int)->int:
+def recursive_search(pos: Tuple, num: int, rule: Callable) -> int:
     sum = 0
     for y, x in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
         new = (pos[0] + y, pos[1] + x)
@@ -35,20 +35,40 @@ def recursive_search(pos: Tuple, num: int)->int:
             continue
         new_num = int(data[new[0]][new[1]])
         if new_num == num + 1:
-            if new in explored_1:
+            if rule(new, pos):
                 continue
-            explored_1.add(new)
+            # if new in explored_1:
+            #     continue
+            # explored_1.add(new)
             if new_num == 9:
                 sum += 1
                 continue
-            sum += recursive_search(new, new_num)
+            sum += recursive_search(new, new_num, rule)
     return sum
+
+
+def rule_1(new, pos) -> bool:
+    if new in explored_1:
+        return True
+    explored_1.add(new)
+    return False
+
+
+def rule_2(new, pos) -> bool:
+    return False
+    # if (pos, new) in explored_2:
+    #     return True
+    # explored_2.add((pos, new))
+    # return False
 
 
 for start in starts:
     explored_1 = set()
-    trails = recursive_search(start, 0)
-    ans_1 += trails
+    trails_1 = recursive_search(start, 0, rule_1)
+    ans_1 += trails_1
+    explored_2 = set()
+    trails_2= recursive_search(start, 0, rule_2)
+    ans_2 += trails_2
 
 
 print(f"1: {ans_1}")
