60bc2e1d2f0f2ac179964ee3c2485d9278005bff
Julius Franke <julius.franke@gmx.net>
2024-12-23 11:10:19 +0100

    day 23.2

--- 23.py
+++ 23.py
@@ -14,26 +14,31 @@ input = input if len(sys.argv) == 1 else example
 with open(input, "r") as file:
     data = file.read().strip().splitlines()
 
-conns = defaultdict(list)
+comps = set()
+conns = set()
 for line in data:
     c1, c2 = line.split("-")
-    conns[c1].append(c2)
-    conns[c2].append(c1)
+    comps.update((c1, c2))
+    conns.update(((c1, c2), (c2, c1)))
 
-
+print(conns)
 ans_1 = 0
-for k1, k2, k3 in combinations(list(conns.keys()), 3):
-    if k1 not in conns[k2] or k1 not in conns[k3]:
-        continue
-    if k2 not in conns[k1] or k2 not in conns[k3]:
-        continue
-    if k3 not in conns[k1] or k3 not in conns[k2]:
+for k1, k2, k3 in combinations(comps, 3):
+    if not {(k1, k2), (k2, k3), (k1, k3)} < conns:
         continue
     if "t" not in (k1[0] + k2[0] + k3[0]):
         continue
     ans_1 += 1
 
-ans_2 = 0
+lans = [{k} for k in comps]
+
+for lan in lans:
+    for k1 in comps:
+        if all((k1, k2) in conns for k2 in lan):
+            lan.add(k1)
+
+ans_2 = sorted(max(lans, key=len))
+
 
 print(f"1: {ans_1}")
-print(f"2: {ans_2}")
+print(f"2: {','.join(ans_2)}")
